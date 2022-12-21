d94928a5bb1904a3b1a6178663912ccabf2e3e98
stanley <cjstanenemuo@yahoo.com>
2022-12-21 17:44:33 -0500

    intial commit

---
+++ server/models/Sauce.js
@@ -0,0 +1,26 @@
+const { sequelize, DataTypes } = require("../db");
+
+const Sauce = sequelize.define("sauce", {
+  name: {
+    name: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  category: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  image: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = { Sauce };
